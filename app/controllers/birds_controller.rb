f7329940b45bf0dfc94a64562f0c94c4c4039a43
nickwells <wellsnick11@yahoo.com>
2022-08-02 11:04:25 -0700

    commit tree 9b89330e164c7224d8d1b70c9cc4130bb9968b72

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,11 +8,10 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
-
-  # GET /birds/:id
+   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
     if bird
@@ -21,5 +20,11 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+  private 
+
+    def bird_params
+      params.permit(:name, :species)
+    end
 
 end
